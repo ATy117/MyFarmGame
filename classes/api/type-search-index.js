07e435b311ca5929504c3160b7df9748815f8c01
Gabriel Tan <gabtan99@gmail.com>
2019-01-23 11:31:25 +0800

    removed files first

--- classes/api/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"Farmer"},{"p":"<Unnamed>","l":"Flower"},{"p":"<Unnamed>","l":"FruitTree"},{"p":"<Unnamed>","l":"Seed"},{"p":"<Unnamed>","l":"Tile"},{"p":"<Unnamed>","l":"Vegetable"},{"p":"<Unnamed>","l":"View"}]
